1eecc70a4f79ebec040e7c42fb83c6de02c9e068
FRon17 <120887483+FRon17@users.noreply.github.com>
2024-03-12 21:24:02 +0800

    JS

---
+++ week3/script.js
@@ -0,0 +1,13 @@
+document.write("hello world!")
+
+function sayHello(){
+  var response = prompt("what is ur name?");
+  alert("hello " + response + ", have a nice day~")
+};
+//sayHello();
+
+function showTopic(){
+  var x = document.getElementById('demo');
+  x.style.fontSize = "25px";
+  x.style.color = "red";
+};
